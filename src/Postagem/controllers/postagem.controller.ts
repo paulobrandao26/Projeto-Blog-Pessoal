d4509589b4ef134691264389e54c6dafede175e0
Paulo Henrique de Barros Brandão <paulobrandaohbb@gmail.com>
2026-01-21 15:04:18 -0300

    atualição da parte do modulo Auth part2 e part3

--- src/Postagem/controllers/postagem.controller.ts
+++ src/Postagem/controllers/postagem.controller.ts
@@ -9,10 +9,13 @@ import {
   ParseIntPipe,
   Post,
   Put,
+  UseGuards,
 } from '@nestjs/common';
+import { JwtAuthGuard } from '../../Auth/guard/jwt-auth.guard';
 import { Postagem } from '../entities/Postagem.entity';
 import { PostagemService } from '../services/postagem.service';
 
+@UseGuards(JwtAuthGuard)
 @Controller('/postagens')
 export class PostagemController {
   constructor(private readonly postagemService: PostagemService) {}
@@ -22,26 +25,31 @@ export class PostagemController {
   findAll(): Promise<Postagem[]> {
     return this.postagemService.findAll();
   }
+
   @Get('/:id')
   @HttpCode(HttpStatus.OK)
   findById(@Param('id', ParseIntPipe) id: number): Promise<Postagem> {
     return this.postagemService.findById(id);
   }
+
   @Get('/titulo/:titulo')
   @HttpCode(HttpStatus.OK)
-  findByAllTitulo(@Param('titulo') titulo: string): Promise<Postagem[]> {
+  findAllByTitulo(@Param('titulo') titulo: string): Promise<Postagem[]> {
     return this.postagemService.findByIdTitulo(titulo);
   }
+
   @Post()
   @HttpCode(HttpStatus.CREATED)
   create(@Body() postagem: Postagem): Promise<Postagem> {
     return this.postagemService.create(postagem);
   }
+
   @Put()
   @HttpCode(HttpStatus.OK)
   update(@Body() postagem: Postagem): Promise<Postagem> {
     return this.postagemService.update(postagem);
   }
+
   @Delete('/:id')
   @HttpCode(HttpStatus.NO_CONTENT)
   delete(@Param('id', ParseIntPipe) id: number) {
