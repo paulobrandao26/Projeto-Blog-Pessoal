cf1645562aa8639377b390756ba9ab1ff8b8a22a
Paulo Henrique de Barros Brandão <paulobrandaohbb@gmail.com>
2026-02-14 17:47:35 -0300

    Deploy do Projeto Blog Pessoal

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,25 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AuthModule } from './Auth/auth.module';
-import { ProdService } from './Auth/data/prod.service';
+import { Postagem } from './Postagem/entities/Postagem.entity';
 import { PostagemModule } from './Postagem/Postagem.module';
+import { Tema } from './tema/entities/tema.entity';
 import { TemaModule } from './tema/tema.module';
+import { Usuario } from './usuario/entities/usuario.entity';
 import { UsuarioModule } from './usuario/usuario.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRootAsync({
-      useClass: ProdService,
-      imports: [ConfigModule],
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'db_blogpessoal',
+      entities: [Postagem, Tema, Usuario],
+      synchronize: true,
     }),
     PostagemModule,
     TemaModule,
